35c2282cc195ba10135dcc403ce963d5a3a81635
Pau-za <molo_pau@hotmail.com>
2019-06-10 11:25:30 -0500

    agrega navegación a través del historial del navegador (atrás y adelante :D)

--- src/vews/Pictures.vue
+++ src/vews/Pictures.vue
@@ -1,34 +1,44 @@
 <template>
-    <div>
-        <h1>Fotografías</h1>
-        <router-link :to="{name: 'pictures', params: {id: pic}}" 
-        v-for="(pic,index) of picsArr" :key="index">
-            <button>Foto {{ pic }}</button>
-        </router-link>
-        <Dinamicpics/>
-        <button @click="home">Volver al inicio</button>
-    </div>
+  <div>
+    <h1>Fotografías</h1>
+    <router-link
+      :to="{name: 'pictures', params: {id: pic}}"
+      v-for="(pic,index) of picsArr"
+      :key="index"
+    >
+      <button>Foto {{ pic }}</button>
+    </router-link>
+    <Dinamicpics/>
+    <button @click="home">Volver al inicio</button>
+    <button @click="previus">Atrás</button>
+    <button @click="next">Adelante</button>
+  </div>
 </template>
 
 <script>
-
-import  Dinamicpics  from "@/components/Dinamicpics";
+import Dinamicpics from "@/components/Dinamicpics";
 
 export default {
-    name: "pictures",
-    components:{
-        Dinamicpics
+  name: "pictures",
+  components: {
+    Dinamicpics
+  },
+  data() {
+    return {
+      picsArr: [1, 2, 3, 4]
+    };
+  },
+  methods: {
+    home() {
+      this.$router.push("/");
     },
-    data(){
-        return{
-            picsArr: [1,2,3,4]
-        }
+    previus(){
+        this.$router.go(-1);
     },
-    methods:{
-        home(){
-        this.$router.push('/')
-        }
+    next(){
+        this.$router.go(1);
     }
-}
+  }
+};
 </script>
 
